4ea18eeb143cd8dcac50820c2b2893aed21c480b
okelomax <99961670+okelomax@users.noreply.github.com>
2022-05-31 15:22:19 +0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,42 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * check_cycle - checks if a singly linked list
+ * loops into itself
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a loop
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *nd1;
+	listint_t *nd2;
 
-	if (!list)
-		return (0);
-
-	while (slow && fast && fast->next)
+	nd1 = list;
+	nd2 = list;
+	while (list && nd1 && nd1->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (slow == fast)
+		list = list->next;
+		nd1 = nd1->next->next;
+
+		if (list == nd1)
+		{
+			list = nd2;
+			nd2 =  nd1;
+			while (1)
+			{
+				nd1 = nd2;
+				while (nd1->next != list && nd1->next != nd2)
+				{
+					nd1 = nd1->next;
+				}
+				if (nd1->next == list)
+					break;
+
+				list = list->next;
+			}
 			return (1);
+		}
 	}
 
 	return (0);
