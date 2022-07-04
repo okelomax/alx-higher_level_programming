6404b5129af888ff4e3e9d10fd84d59883814798
okelomax <99961670+okelomax@users.noreply.github.com>
2022-07-04 14:52:41 +0300

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-"""Module 1-my_list.
-Creates a class inheriting from the list class.
+"""Module 10-square.
+Creates a Square class.
 """
 
 
-class MyList(list):
-    """Class MyList inherits from list."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
-    def print_sorted(self):
-        """Prints the list, in ascending sort."""
 
-        new_list = self[:]
-        new_list.sort()
-        print("{}".format(new_list))
+class Square(Rectangle):
+    """Represents a square.
+    Private instance attribute size.
+    Public method area().
+    Inherits from Rectangle.
+    """
+
+    def __init__(self, size):
+        """Initializes a Square.
+        Args:
+            - size: size of the square
+        """
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return super().__str__()
+
+    def area(self):
+        """Computes the area of a Square instance.
+        Overwrites the area() method from Rectangle.
+        """
+
+        return self.__size ** 2
