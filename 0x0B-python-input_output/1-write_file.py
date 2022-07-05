f43378380b7339fbfaf16c79a743c2fe9efa8839
okelomax <99961670+okelomax@users.noreply.github.com>
2022-07-05 22:50:43 +0300

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""
-This file contains functiom
-that returns the number of lines
-of a text file
+"""Module 1-number_of_lines.
+Counts number of lines in a file.
 """
 
 
 def number_of_lines(filename=""):
+    """Counts lines in filename.
+    Args:
+        - filename: name of the file
+    Returns:
+        - number of lines
     """
-    function returns number of lines
-    """
-    i = 0
-    with open(filename, encoding="utf-8") as myFile:
-        for line in myFile:
-            i += 1
-    return (i)
+
+    count = 0
+
+    with open(filename) as f:
+        text = f.readlines()
+        for line in text:
+            count += 1
+
+    return count
