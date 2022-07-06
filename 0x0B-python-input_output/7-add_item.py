4fc5a8c19ecdbe26e8db7f9281ad76b91736639d
okelomax <99961670+okelomax@users.noreply.github.com>
2022-07-06 07:39:35 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-"""Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
+import json
+from sys import argv
+"""
+This file contains a function that adds
+all arguments to a python list and saves
+to a file
 """
 
-import sys
-import json
-import os.path
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
 
-my_file = 'add_item.json'
+filename = 'add_item.json'
 
-my_list = []
+try:
+    new = load_json(filename)
+except (ValueError, FileNotFoundError):
+    new = []
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
+new = new + argv[1:]
+save_json(new, filename)
